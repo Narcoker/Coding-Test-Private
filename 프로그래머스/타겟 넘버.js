4d0458211947f4ed4e0ed28d92f3711e1e36751a
Narcoker <jykim9868@gmail.com>
2022-09-07 14:32:14 +0900

    Solved: [프로그래머스] 타켓 넘버

---
+++ 프로그래머스/타겟 넘버.js
@@ -0,0 +1,16 @@
+//[프로그래머스] 타켓 넘버
+
+let answer = 0;
+
+function dfs(numbers, target, sum, depth) {
+    if (depth < numbers.length) {
+        dfs(numbers, target, sum + numbers[depth], depth + 1);
+        dfs(numbers, target, sum - numbers[depth], depth + 1);
+    }
+    if (depth === numbers.length && sum === target) answer++;
+}
+
+function solution(numbers, target) {
+    dfs(numbers, target, 0, 0);
+    return answer;
+}
