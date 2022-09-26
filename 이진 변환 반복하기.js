bac28a7694ae5404d20d2efc66b6d619ef41d687
Narcoker <jykim9868@gmail.com>
2022-09-26 13:36:28 +0900

    [프로그래머스] 이진 변환 반복하기

---
+++ 이진 변환 반복하기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let zeroCount = 0;
+    let changeCount = 0;
+    let nextS;
+
+    while (s !== '1') {
+        nextS = s.split("0").join('')
+        zeroCount += s.length - nextS.length;
+        s = nextS.length.toString(2);
+        changeCount++;
+    }
+
+    return [changeCount, zeroCount];
+}
