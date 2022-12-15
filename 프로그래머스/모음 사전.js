098d6c0668d268ce9e3d76f3761548d06f5884ca
Narcoker <jykim9868@gmail.com>
2022-12-15 18:32:48 +0900

    Solved: [프로그래머스] 모음 사전

--- 프로그래머스/모음 사전.js
+++ 프로그래머스/모음 사전.js
@@ -1,15 +1,16 @@
 function solution(word) {
-    function getResult(str) {
+    function isCorrect(str) {
         if (str === word) return true;
         if (str.length === 5) return false;
         for (let i = 0; i < words.length; i++) {
             answer++;
-            if (getResult(str + words[i])) return true;
+            if (isCorrect(str + words[i])) {
+                return true;
+            }
         }
-        return false;
     }
     let answer = 0;
     let words = ["A", "E", "I", "O", "U"];
-    getResult("");
+    isCorrect("");
     return answer;
 }
