ed3db9ad166149226e49cfbc714343fdce2e7eb3
Narcoker <jykim9868@gmail.com>
2023-02-07 14:50:33 +0900

    Solved: [프로그래머스] 둘만의 암호

---
+++ 프로그래머스/둘만의 암호.js
@@ -0,0 +1,15 @@
+function solution(s, skip, index) {
+    let answer = '';
+    let skipAlpha = "abcdefghijklmnopqrstuvwxyz";
+
+    for (let s of skip)
+        skipAlpha = skipAlpha.replace(s, "");
+    let tripleSkipAlpha = skipAlpha + skipAlpha + skipAlpha;
+
+    for (let targetChar of s) {
+        let changeChar = tripleSkipAlpha.at(tripleSkipAlpha.indexOf(targetChar) + index);
+        answer += changeChar;
+    }
+    return answer;
+}
+
