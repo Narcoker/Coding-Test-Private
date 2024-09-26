820508f1336c78aaf9c2b9cda3a18e6553a833f1
Narcoker <jykim9868@gmail.com>
2024-09-26 14:16:33 +0900

    Solved: [백준] 11053번: 가장 긴 증가하는 부분수열

--- 백준/11053.js
+++ 백준/11053.js
@@ -1,15 +1,36 @@
-const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-let n = Number(input.shift());
-input = input[0].split(' ').map(Number);
+const path =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "\\data.txt";
+const input = require("fs").readFileSync(path).toString().split("\n");
 
-let result = new Array(n).fill(1);
-for (let targetIndex = 0; targetIndex < n; targetIndex++) {
-    for (let index = 0; index < targetIndex; index++) {
-        if (input[targetIndex] > input[index]) {
-            result[targetIndex] = Math.max(result[targetIndex], result[index] + 1);
-        }
+let [N, arr] = [Number(input[0]), input[1].split(" ").map(Number)];
+
+function solution(N, arr) {
+  const dp = new Array(N).fill(1);
+  for (let curIndex = 1; curIndex < N; curIndex++) {
+    for (let preIndex = 0; preIndex < curIndex; preIndex++) {
+      if (arr[preIndex] < arr[curIndex]) {
+        dp[curIndex] = Math.max(dp[curIndex], dp[preIndex] + 1);
+      }
     }
+  }
+  return Math.max(...dp);
 }
 
-console.log(Math.max(...result));
+const answer = solution(N, arr);
+console.log(answer);
+
+// const fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let n = Number(input.shift());
+// input = input[0].split(' ').map(Number);
+
+// let result = new Array(n).fill(1);
+// for (let targetIndex = 0; targetIndex < n; targetIndex++) {
+//     for (let index = 0; index < targetIndex; index++) {
+//         if (input[targetIndex] > input[index]) {
+//             result[targetIndex] = Math.max(result[targetIndex], result[index] + 1);
+//         }
+//     }
+// }
+
+// console.log(Math.max(...result));
